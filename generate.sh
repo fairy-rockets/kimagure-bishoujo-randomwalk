1203928e5f3d076e5f4d12ced86363b6a2f6ba3b
psi <psi@ledyba.org>
2017-09-06 10:05:00 +0900

    gen dir -p

--- generate.sh
+++ generate.sh
@@ -7,7 +7,7 @@ for ((i=0; i < 15; i++)); do
   {
     ${CHROME} --headless --disable-gpu --remote-debugging-port=$((10000 + $i)) 'http://localhost:8080' &
     sleep 5
-    mkdir $(printf "%04d" $i)
+    mkdir -p $(printf "%04d" $i)
     node main.js $i $((10000 + $i)) ${MAX} &
   } &
 done
