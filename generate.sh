1f0935fa5ca9358f73ed99d08721de366160d028
psi <psi@ledyba.org>
2017-09-06 10:09:21 +0900

    gen dir

--- generate.sh
+++ generate.sh
@@ -1,14 +1,18 @@
 #! /bin/bash
 
+set -eu
+
 CHROME=google-chrome
 MAX=14400
 
 for ((i=0; i < 15; i++)); do
   {
+    set -x
     ${CHROME} --headless --disable-gpu --remote-debugging-port=$((10000 + $i)) 'http://localhost:8080' &
     sleep 5
-    mkdir -p $(printf "%04d" $i)
+    mkdir -p $(printf "img%04d" $i)
     node main.js $i $((10000 + $i)) ${MAX} &
+    set +x
   } &
 done
 
